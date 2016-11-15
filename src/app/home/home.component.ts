6e8db1e251242bef20e49d393e3418ca5b4c9093
Aliaksei Habruseu <aliaksei.habruseu@aerlingus.com>
2016-11-15 18:24:38 +0300

    Observer pattern

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 
+class Click {
+  private handlers: Array<{context: any, fn: any}>;
+  constructor() {
+    this.handlers = [];
+  }
+  subscribe(fn, context?) {
+    context = context || window;
+    this.handlers.push({context, fn});
+  }
+  unsubscribe(context) {
+    this.handlers = this.handlers.filter(handler => {
+      if (handler.context !== context) return handler; 
+    });
+  }
+  fire(message) {
+    for (let handler of this.handlers) {
+      handler.fn.call(handler.context, message);
+    }
+  }
+}
+
+class Observer {
+  private messsages: Array<string>;
+  private id;
+  public subscribed: boolean = false;
+  constructor(id: number) {
+    this.messsages = [];
+    this.id = id;
+  }
+  log(msg) {
+    this.messsages.push(this.toString() + ':' + msg);
+  }
+  getMessages(): Array<string> {
+    return this.messsages;
+  }
+  toString() {
+    return "Observer #" + this.id;
+  }
+  getLastMessage(): string {
+    return this.messsages.length > 0 ? this.messsages[this.messsages.length-1] : null;
+  }
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  observer1: Observer;
+  observer2: Observer;
+  subject: Click;
+
+  constructor() {
+    this.subject = new Click();
+    this.observer1 = new Observer(1);
+    this.observer2 = new Observer(2);
+  }
 
   ngOnInit() {
+
+    // subject.subscribe(observer1.log, observer1);
+    // subject.subscribe(observer2.log, observer2);
+
+    // subject.fire('Message A');    
+    // console.log(observer1.getMessages());
+    // console.log(observer2.getMessages());
+    // console.log('--------------------------------');
+    // subject.unsubscribe(observer1);
+
+    // subject.fire('Message B');    
+    // console.log(observer1.getMessages());
+    // console.log(observer2.getMessages());
+
+  }
+
+  fireEvent(msg: string) {
+    this.subject.fire(msg);
+  }
+
+  switchSubscribtion(observer: Observer) {
+    observer.subscribed = !observer.subscribed;
+    if (observer.subscribed) {
+      this.subject.subscribe(observer.log, observer);
+    } else {
+      this.subject.unsubscribe(observer);
+    }
   }
 
 }
